b0bdf88e023ae99a5dc0f21529909950f94aa31e
Alberto Kato <albertokatojr@gmail.com>
2022-06-14 20:02:47 -0300

    improving coverage...

--- Tests/VehiclesAppTests/RespositoryTests.cs
+++ Tests/VehiclesAppTests/RespositoryTests.cs
@@ -49,5 +49,49 @@ namespace VehiclesAppTests.Repository
             //Assert
             Assert.Equal(expectedColor, actualColor);
         }
+        
+        [Fact(DisplayName = "GetAll")]
+        public void Should_GetAll_Vehicles()
+        {
+
+            //Act
+            var vehicles = Repository.GetAll();
+
+            //Assert
+            Assert.NotNull(vehicles);
+            Assert.NotEmpty(vehicles);
+        }
+
+        [Fact(DisplayName = "Get")]
+        public void Should_Get_A_Vehicle()
+        {
+            //Arrange
+            var id = new Guid("e335a290-db4d-412c-abb5-90074d5d65e5");
+
+            //Act
+            var vehicle = Repository.Get(id);
+
+            //Assert
+            Assert.NotNull(vehicle);
+            Assert.IsType<Plane>(vehicle);
+            Assert.Equal(id, vehicle.Id);
+        }
+
+
+        [Fact(DisplayName = "Delete")]
+        public void Should_Delete_A_Vehicle()
+        {
+            //Arrange
+            var plane = new Plane(2009, "Blue", 6);
+            Repository.Insert(plane);
+            var expectedCount = Repository.GetAll().Count() - 1;
+
+            //Act
+            Repository.Delete(plane.Id);
+            var actualCount = Repository.GetAll().Count();
+
+            //Assert
+            Assert.Equal(expectedCount, actualCount);
+        }
     }
 }
---
+++ Tests/VehiclesAppTests/VehicleTests.cs
@@ -0,0 +1,37 @@
+﻿using VehiclesApp.Model;
+
+namespace VehiclesAppTests
+{
+    public class VehicleTests
+    {
+
+        [Fact(DisplayName = "CarMove")]
+        public void Should_Validate_Car_Move()
+        {
+            //Arrange
+            var car = new Car(2009, "Black");
+            var expectedMove = "Driving...";
+
+            //Act
+            var actualMove = car.Move();
+
+            //Assert
+            Assert.Equal(expectedMove, actualMove);
+        }
+
+
+        [Fact(DisplayName = "PlaneMove")]
+        public void Should_Validate_Plane_Move()
+        {
+            //Arrange
+            var plane = new Plane(2009, "Black", 5);
+            var expectedMove = "Flying...";
+
+            //Act
+            var actualMove = plane.Move();
+
+            //Assert
+            Assert.Equal(expectedMove, actualMove);
+        }
+    }
+}
